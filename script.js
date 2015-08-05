686b141fdb265cee6e37c255fb6697cbbad7a968
Loucasse <takazyt@gmail.com>
2015-08-05 15:12:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 $('#taigachat_motd').after('<center><p><span style="color: orange;"><i class="fa fa-star fa-spin"></i></span> <a href="https://realitygaming.fr/conversations/add?to=Loucasse+GTP,Veenox+GTP&title=Demande de notice premium" title="Demander son topic en notice" target="_blank" style="color: orange; font-weight: bold;">Notice Premium :</span></a> <a href="https://realitygaming.fr/threads/console-id-partage-premium-20-04-2015.433917/" target="_blank" style="color: black;">Console ID partagé</a> ~ <a href="https://realitygaming.fr/threads/les-regles-dor-de-la-musculation.463960/" target="_blank" style="color: black;">Les règles en or pour la musculation</a></p>')
+
+
+if($('a.crumb').eq(1).attr('href') === 'https://realitygaming.fr/categories/zone-premium.66/'){
+if($('.RewardLabel').text() === 'Demander une récompense'){
+bouton = setInterval(function(){ if (($('#XenForoUniq0').css('display')) === 'block'){
+clearInterval(bouton);
+$('a[href$="/notice"]').after('<a href="https://realitygaming.fr/conversations/add?to=Loucasse+GTP,Veenox+GTP&amp;title=Demande%20de%20notice%20premium" title="Demander son topic en notice" class="NoticeLink OverlayTrigger item control" data-container="#notice-post-"><i="" style="color: orange;" class="fa fa-star fa-spin"></i=""><span class="NoticeLabel">    Demander en notice Premium</span></a>');
+};},1);}}else{return;}
